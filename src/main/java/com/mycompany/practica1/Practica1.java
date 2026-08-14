9c3e00dd87335fb2e589e28808f13753f3ae756c
valeska <valeskaaparicio5@gmail.com>
2026-08-14 00:02:38 -0600

    Creación del menú principal

---
+++ src/main/java/com/mycompany/practica1/Practica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1;
+
+/**
+ *
+ * @author vales
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
