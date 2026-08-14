ee0ff343c8e3cef9bb1d8c66639369bb684b5500
valeska <valeskaaparicio5@gmail.com>
2026-08-14 11:11:13 -0600

    actualización menú

--- src/main/java/com/mycompany/practica1/Practica1.java
+++ src/main/java/com/mycompany/practica1/Practica1.java
@@ -1,16 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.practica1;
 
-/**
- *
- * @author vales
- */
-public class Practica1 {
+ import java.util.Scanner;
 
+public class Practica1 {
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    Scanner sc = new Scanner(System.in);
+    menu (sc);
+}
+
+//método para mostrar menú
+    public static void menu(Scanner sc){
+   
+    System.out.println("======SISTEMA DE ESTACIONAMIENTO======")
+    System.out.println("1. Ingrese Vehículo")
+    System.out.println("2. Retirar Vehículo")
+    System.out.println("3. Mostrar estacionamiento")
+    System.out.println("4. Buscar vehículo por placa")
+    System.out.println("5. Mostrar ruta más corta entre entrada y salida") 
+    System.out.println("6. Mostrar ingresos")
+    System.out.println("7. Salir")
     }
 }
