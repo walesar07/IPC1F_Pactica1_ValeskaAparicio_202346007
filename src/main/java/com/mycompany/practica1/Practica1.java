b7e68425634b490fd2e8d40aff28fa25bb5a3b86
valeska <valeskaaparicio5@gmail.com>
2026-08-14 12:53:38 -0600

    Creacion método inicializar y mostrar estacionamiento

--- src/main/java/com/mycompany/practica1/Practica1.java
+++ src/main/java/com/mycompany/practica1/Practica1.java
@@ -5,21 +5,52 @@ package com.mycompany.practica1;
 
 public class Practica1 {
     
+    //arreglo representa estacionamiento
+    static char [][] estacionamiento = new char [10][10];
+    
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    menu (sc);
-}
-
-//método para mostrar menú
-    public static void menu(Scanner sc){
-   
-    System.out.println("======SISTEMA DE ESTACIONAMIENTO======")
-    System.out.println("1. Ingrese Vehículo")
-    System.out.println("2. Retirar Vehículo")
-    System.out.println("3. Mostrar estacionamiento")
-    System.out.println("4. Buscar vehículo por placa")
-    System.out.println("5. Mostrar ruta más corta entre entrada y salida") 
-    System.out.println("6. Mostrar ingresos")
-    System.out.println("7. Salir")
+    
+    inicializarEstacionamiento();
+    mostrarEstacionamiento();
+    mostrarMenu (sc);
     }
-}
+    
+//método para mostrar menú
+    public static void mostrarMenu(Scanner sc){
+   
+    System.out.println("======SISTEMA DE ESTACIONAMIENTO======");
+    System.out.println("1. Ingrese Vehículo");
+    System.out.println("2. Retirar Vehículo");
+    System.out.println("3. Mostrar estacionamiento");
+    System.out.println("4. Buscar vehículo por placa");
+    System.out.println("5. Mostrar ruta más corta entre entrada y salida");
+    System.out.println("6. Mostrar ingresos");
+    System.out.println("7. Salir");
+    }
+    //método para inicializar estacionamiento
+    public static void inicializarEstacionamiento(){
+        for (int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                
+               if ( i==0 || i==9 ||j==0 ||j==9 ){
+                   estacionamiento[i][j] = '=';
+                    
+               } else {
+                    estacionamiento [i][j ]= 'L';
+               }
+            }
+        }
+
+    }
+    //metodo para mostrar estacionamiento
+    public static void mostrarEstacionamiento(){
+        for (int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print(estacionamiento[i][j]+" ");         
+            } 
+            System.out.println();
+        } 
+    }
+
+}   
